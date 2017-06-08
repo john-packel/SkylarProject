c6682e740f0b8541bf2bc94d9d8918548e476629
John Packel <john.packel@gmail.com>
2017-06-08 17:02:33 -0400

    express.static

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 
-app.use(express.static(__dirname + '/index.jade'));
+app.use(express.static('public'));
 
 var port = process.env.PORT || 8080;
 
 app.listen(port);
+console.log('listening on port: ', port);
